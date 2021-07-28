b654d8ada2d8e52c27c444402771da64963353fc
will cole <will.cole@willistowerswatson.com>
2021-07-28 16:07:51 +0100

    OptedForJsonParse

--- ExcelTableToJson5.js
+++ ExcelTableToJson5.js
@@ -1,38 +1,36 @@
-function ExcelTableToJSON(rngTable) {
-	
-	var log = ""
-	log = log + "1"
-	
-    try {
-        if (rngTable && rngTable['Rows'] && rngTable['Columns']) {
-            var rowCount = rngTable.Rows.Count;
-            var columnCount = rngTable.Columns.Count;
-            var arr = new Array();
-			var headers = new Array();
-			
-			for (columnLoop = 1; columnLoop <= columnCount; columnLoop++){
-				headers[columnLoop - 1] = rngTable.Cells(1, columnLoop);
-			}
-			
-            for (rowLoop = 2; rowLoop <= rowCount; rowLoop++) {
-                var dict = {};
-                for (columnLoop = 1; columnLoop <= columnCount; columnLoop++) {
-                    var rngCell = rngTable.Cells(rowLoop, columnLoop);
-					if(rngCell.Value2  != "" && rngCell.Value2  != "NULL")
+function ExcelTableToJSON(rngTable)
+		{
+			try {
+				if (rngTable && rngTable['Rows'] && rngTable['Columns']) {
+					var rowCount = rngTable.Rows.Count;
+					var columnCount = rngTable.Columns.Count;
+					var arr = new Array();
+					
+					var headers = new Array();
+					for (i = 1; i <= columnCount; i++)
 					{
-						dict[headers[columnLoop - 1]] = rngCell.Value2;
+						var rngCell = rngTable.Cells(1, columnLoop);
+						headers[i] = rngCell.Value2;
 					}
-                }
-				arr[rowLoop - 2] = dict;
-            }
-			
-			return JSON.stringify(arr);
-        }
-        else {
-            return { error: '#Either rngTable is null or does not support Rows or Columns property!' };
-        }
-    }
-    catch(err) {
-        return {error: err.message};
-    }
-}
+					
+					for (rowLoop = 1; rowLoop <= rowCount; rowLoop++) {
+						arr[rowLoop - 1] = {};
+						//arr[rowLoop - 1] = new Array();
+						// Add a coll header
+						for (columnLoop = 1; columnLoop <= columnCount; columnLoop++) {
+							var rngCell = rngTable.Cells(rowLoop, columnLoop);
+							var cellValue = rngCell.Value2;
+							var keyValue = header[columnLoop];
+							arr[rowLoop - 1][keyValue] = cellValue; //[columnLoop - 1] = cellValue;
+						}
+					}
+					return JSON.parse(arr) //[4]; //JSON.stringify(data);
+				}
+				else {
+					return { error: '#Either rngTable is null or does not support Rows or Columns property!' };
+				}
+			}
+			catch(err) {
+				return {error: err.message};
+			}
+		}
